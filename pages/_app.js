820efe08f499ba695d07eeaff98800a7daa777a3
/lifecats <58966204+lifecats@users.noreply.github.com>
2022-12-12 16:46:30 +0000

    i focunt something on github lets hope it works

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,26 @@ import "react-toastify/dist/ReactToastify.css";
 import { useEffect, useState } from "react";
 import { AnimatePresence, domAnimation, LazyMotion, m } from "framer-motion";
 
+import Router from "next/router";
+
+const routeChange = () => {
+  // Temporary fix to avoid flash of unstyled content
+  // during route transitions. Keep an eye on this
+  // issue and remove this code when resolved:
+  // https://github.com/vercel/next.js/issues/17464
+
+  const tempFix = () => {
+    const allStyleElems = document.querySelectorAll('style[media="x"]');
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute("media");
+    });
+  };
+  tempFix();
+};
+
+Router.events.on("routeChangeComplete", routeChange);
+Router.events.on("routeChangeStart", routeChange);
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
