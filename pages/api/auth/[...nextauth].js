54e8a8b90eb01756c9d58385289a7e7a53c23331
/lifecats <58966204+lifecats@users.noreply.github.com>
2022-12-15 07:47:36 +0000

    fix secret error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,7 +7,10 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+
 };
 export default NextAuth(authOptions);
