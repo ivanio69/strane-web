bb6a2bc19c915936979ea052669d67820eba7cc1
/lifecats <58966204+lifecats@users.noreply.github.com>
2022-12-19 16:33:52 +0000

    animate auth action page transition

--- pages/auth/[action].js
+++ pages/auth/[action].js
@@ -35,97 +35,111 @@ function Next() {
       <Link href="/">
         <p className={styles.back}> {"<"}</p>
       </Link>
-
-      <div className={styles.container}>
-        <div className={styles.left}>
-          <h1 className={styles.title}>
-            <span>S</span>trane<span>.</span>
-          </h1>
-          {action == "signin" ? (
-            <h2 className={styles.create}>Sign In</h2>
-          ) : action == "signup" ? (
-            <h2 className={styles.create}>Create account</h2>
-          ) : (
-            <p></p>
-          )}
-        </div>
-
-        <LazyMotion features={domAnimation}>
-          <AnimatePresence mode="wait">
-            <m.div
-              key={
-                animation.name +
-                JSON.stringify(loading) +
-                JSON.stringify(serverVerified)
-              }
-              className={styles.right}
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              variants={animation.variants}
-              transition={animation.transition}
-              opacity={0}
-            >
-              {loading ? (
-                <Grid
-                  height="80"
-                  width="80"
-                  color="#6349ff"
-                  ariaLabel="grid-loading"
-                  radius="12.5"
-                  wrapperStyle={{}}
-                  wrapperClass=""
-                  visible={true}
-                />
-              ) : (
-                <>
-                  {!serverVerified ? (
-                    <ServerInfo state={setServerVerified} />
-                  ) : (
-                    <>
-                      {loggedIn ? (
-                        <LoggedIn />
-                      ) : (
-                        <>
-                          {action == "signin" ? (
-                            <SignIn />
-                          ) : action == "signup" ? (
-                            <SignUp />
-                          ) : (
-                            <p>
-                              Sorry, action that you requested is not available
-                            </p>
-                          )}
-                        </>
-                      )}
-                    </>
-                  )}
-                </>
-              )}
-            </m.div>
-          </AnimatePresence>
-        </LazyMotion>
-        <div className={styles.lb} />
-        {loggedIn ? null : (
-          <div
-            className={styles.googlebox}
-            onClick={() => {
-              signIn("google");
-            }}
+      <LazyMotion features={domAnimation}>
+        <AnimatePresence mode="wait">
+          <m.div
+            key={action}
+            className="page-wrap"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={animation.variants}
+            transition={animation.transition}
+            opacity={0}
           >
-            <span>Or use authentication provider:</span>{" "}
-            <div className={styles.item}>
-              <Image src="/icons/google.svg" width={30} height={30} />{" "}
-              <span>Google</span>
+            <div className={styles.container}>
+              <div className={styles.left}>
+                <h1 className={styles.title}>
+                  <span>S</span>trane<span>.</span>
+                </h1>
+                {action == "signin" ? (
+                  <h2 className={styles.create}>Sign In</h2>
+                ) : action == "signup" ? (
+                  <h2 className={styles.create}>Create account</h2>
+                ) : (
+                  <p></p>
+                )}
+              </div>
+              <LazyMotion features={domAnimation}>
+                <AnimatePresence mode="wait">
+                  <m.div
+                    key={
+                      animation.name +
+                      JSON.stringify(loading) +
+                      JSON.stringify(serverVerified)
+                    }
+                    className={styles.right}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    variants={animation.variants}
+                    transition={animation.transition}
+                    opacity={0}
+                  >
+                    {loading ? (
+                      <Grid
+                        height="80"
+                        width="80"
+                        color="#6349ff"
+                        ariaLabel="grid-loading"
+                        radius="12.5"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                        visible={true}
+                      />
+                    ) : (
+                      <>
+                        {!serverVerified ? (
+                          <ServerInfo state={setServerVerified} />
+                        ) : (
+                          <>
+                            {loggedIn ? (
+                              <LoggedIn />
+                            ) : (
+                              <>
+                                {action == "signin" ? (
+                                  <SignIn />
+                                ) : action == "signup" ? (
+                                  <SignUp />
+                                ) : (
+                                  <p>
+                                    Sorry, action that you requested is not
+                                    available
+                                  </p>
+                                )}
+                              </>
+                            )}
+                          </>
+                        )}
+                      </>
+                    )}
+                  </m.div>
+                </AnimatePresence>
+              </LazyMotion>
+              <div className={styles.lb} />
+              {loggedIn ? null : (
+                <div
+                  className={styles.googlebox}
+                  onClick={() => {
+                    signIn("google");
+                  }}
+                >
+                  <span>Or use authentication provider:</span>{" "}
+                  <div className={styles.item}>
+                    <Image src="/icons/google.svg" width={30} height={30} />{" "}
+                    <span>Google</span>
+                  </div>
+                </div>
+              )}
+              <div className={styles.lb} />
+              <p className={styles.tos}>
+                By continuing you accept our standard terms and condition and
+                our privacy policy.{" "}
+              </p>
             </div>
-          </div>
-        )}
-        <div className={styles.lb} />
-        <p className={styles.tos}>
-          By continuing you accept our standard terms and condition and our
-          privacy policy.{" "}
-        </p>
-      </div>
+          </m.div>
+        </AnimatePresence>
+      </LazyMotion>
     </React.Fragment>
   );
 }
