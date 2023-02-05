0eaeaab1af5f1a5b67b4d4e61809d1ec0a267869
/lifecats <58966204+lifecats@users.noreply.github.com>
2023-02-05 12:54:35 +0000

    finish, add newsletter

--- pages/try.js
+++ pages/try.js
@@ -4,7 +4,13 @@ export default function TryBeta() {
   return (
     <p style={{ textAlign: "center", marginTop: "30vh" }}>
       We are really sorry, but the beta testing is yet to begin. Come back
-      later! Bye!
+      later!
+      <br />
+      But now you can{" "}
+      <Link href="http://x57oo.mjt.lu/wgt/x57oo/1tq/form?c=ca2cb789">
+        subscribe to monthly updates about the beta testing:{" "}
+        <b>Strane Development Insights</b>!
+      </Link>
       <br />
       <Link href="/">Go home</Link>
     </p>
